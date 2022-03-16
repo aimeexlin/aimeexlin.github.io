dc7a9b6f27939539d12f53eb7e8885d64c9b8373
aimeexlin <35876705+aimeexlin@users.noreply.github.com>
2022-03-16 23:08:56 +1300

    Revert "updated hovers"
    This reverts commit a8e5f183d9f516422fba756fb4d4328b2da77927.

--- script.js
+++ script.js
@@ -1,8 +1,6 @@
 function showDetails(name) {
     document.getElementById(name).style.display="block";
-    document.getElementById("p"+name).style.backdropFilter="brightness(25%) blur(1px)";
 }
 function hideDetails(name) {
     document.getElementById(name).style.display="none";
-    document.getElementById("p"+name).style.backdropFilter="brightness(75%)";
 }
