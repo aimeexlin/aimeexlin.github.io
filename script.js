a2b11793631cc9c4e4204303472e3e0601a6784f
aimeexlin <35876705+aimeexlin@users.noreply.github.com>
2022-10-29 00:24:57 +1300

    october update

--- script.js
+++ script.js
@@ -1,8 +1,44 @@
 function showDetails(name) {
-    document.getElementById(name).style.display="block";
-    document.getElementById("p"+name).style.backdropFilter="brightness(50%) blur(1px)";
+    var text = document.getElementById(name)
+    var title = document.getElementById("p"+name)
+    var tile = document.getElementById("t"+name)
+    text.style.display="block";
+    title.style.backdropFilter="brightness(50%) blur(1px)";
 }
 function hideDetails(name) {
-    document.getElementById(name).style.display="none";
-    document.getElementById("p"+name).style.backdropFilter="brightness(75%)";
+    var text = document.getElementById(name)
+    var title = document.getElementById("p"+name)
+    var tile = document.getElementById("t"+name)
+    text.style.display="none";
+    title.style.backdropFilter="brightness(75%)";
+}
+function showAll() {
+    var array = document.getElementsByClassName("tile");
+    for (let i = 0; i < array.length; i++) {
+        array[i].style.display="block";
+      }
+    var array2 = document.getElementsByClassName("tag");
+    for (let j = 0; j < array2.length; j++) {
+        array2[j].style.color="white";
+        array2[j].style.backgroundColor="#363434";
+    }
+    document.getElementById("all").style.color="#181717";
+    document.getElementById("all").style.backgroundColor="#B6D7E4";
+}
+function filterBy(name) {
+    var array = document.getElementsByClassName("tile");
+    for (let i = 0; i < array.length; i++) {
+        array[i].style.display="none";
+      }
+    var array2 = document.getElementsByClassName(name);
+    for (let j = 0; j < array2.length; j++) {
+        array2[j].style.display="block";
+    }
+    var array3 = document.getElementsByClassName("tag");
+    for (let k = 0; k < array3.length; k++) {
+        array3[k].style.color="white";
+        array3[k].style.backgroundColor="#363434";
+    }
+    document.getElementById(name).style.color="#181717";
+    document.getElementById(name).style.backgroundColor="#B6D7E4";
 }
